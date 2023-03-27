fdb22c7bfcee5b667276fe556c797dd4e75f4611
Ade Anam <adeca299@gmail.com>
2023-03-27 13:48:48 +0700

    Updates

---
+++ precache-manifest.ff3fafcc0b2688d9225f2af7dd06f745.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e9ecc1ea16afdb4501d",
+    "url": "/adeanam-portfolio/static/css/main.bf44a063.chunk.css"
+  },
+  {
+    "revision": "2e9ecc1ea16afdb4501d",
+    "url": "/adeanam-portfolio/static/js/main.2e9ecc1e.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/adeanam-portfolio/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/adeanam-portfolio/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "2b754154801d33b40252",
+    "url": "/adeanam-portfolio/static/js/runtime~main.2b754154.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/adeanam-portfolio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "315882532d73c1add9ec2073b9a5b9d6",
+    "url": "/adeanam-portfolio/index.html"
+  }
+];
